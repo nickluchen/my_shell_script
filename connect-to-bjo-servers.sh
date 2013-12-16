3a77d4473c9817d83b0efbc59e34e316903586b0
nickluchen <nickluchen@hotmail.com>
2013-12-16 08:48:04 +0800

    Update the connect-to-bjo-servers.sh.

--- connect-to-bjo-servers.sh
+++ connect-to-bjo-servers.sh
@@ -4,20 +4,38 @@ PREFIX=bjo-eng-bld
 SUFFIX=dolby.net
 DEFAULT=3 
 USERNAME=clu
+show_info=1
 
-echo "Will connect to ${PREFIX}-0x servers"
-echo "Please choose the ID of servers, 0 ~ 8 are available"
+if [ "$1" != "" ]
+then
+    index=$1
+else
+	echo "Will connect to ${PREFIX}-0x servers"
+	echo "Please choose the ID of servers, 0 ~ 10 are available"
 
-read index
+	read index
 
-if [ "${index}" == "" ]; then
-    echo "You didn't input anything, will connect to ${PREFIX}-0${DEFAULT} by default"
-    index=${DEFAULT}
+	if [ "${index}" == "" ]; then
+		echo "You didn't input anything, will connect to \
+                  ${PREFIX}-0${DEFAULT} by default"
+		index=${DEFAULT}
+	fi
+    show_info=0
 fi
 
-if [ $((index)) -lt 0 ] || [ $((index)) -gt 8 ]; then
-    echo The ID you input is invalid: ${index}
+if [ $((index)) -lt 0 ] || [ $((index)) -gt 10 ]; then
+	echo The ID you input is invalid: ${index}
+else
+	if [ $((index)) -lt 10 ]
+	then
+		index=0${index}
+	fi
+    if [ $((show_info)) -ne 0 ]
+    then
+        echo "Will connect to ${PREFIX}-${index} server"
+    fi
 fi
+
 
 echo "Please input you login ID, default is ${USERNAME}"
 read user
@@ -27,5 +45,5 @@ if [ "${user}" == "" ]; then
 fi
 
 set -x
-#ssh -X ${user}@${PREFIX}-0${index}.${SUFFIX}
-ssh ${user}@${PREFIX}-0${index}.${SUFFIX}
+#ssh -X ${user}@${PREFIX}-${index}.${SUFFIX}
+ssh ${user}@${PREFIX}-${index}.${SUFFIX}
