d87c0ff0da20f3b974d61baaa6d57c663583385e
nickluchen <nickluchen@hotmail.com>
2014-04-11 12:51:43 +0800

    Some minor changes and fixes.

--- get_size_of_my_home.sh
+++ get_size_of_my_home.sh
@@ -6,22 +6,25 @@ elif [ -n "${USER}" ]; then
     CURRENT_USER=${USER}
 else
     echo "Unknown user."
+    exit -1
 fi
 
 TEMP_FILE=/tmp/${CURRENT_USER}_home_size_temp.txt
 
+echo "HOME is ${HOME}"
 cd ${HOME} || exit 2
 
 echo '##################################'
 echo Today is: `date -R`
 echo '##################################'
 
-du --max-depth=1 -h > ${TEMP_FILE}
-
 echo ========================================
 echo " We are in `pwd -P`"
 echo ----------------------------------------
 #echo -e #print a blank line.
+
+du --max-depth=1 -h > ${TEMP_FILE}
+
 echo Listing by size
 for unit in T G M K; do 
     cat ${TEMP_FILE} | grep -e "[0-9]${unit}"|sort -nr -k 1
