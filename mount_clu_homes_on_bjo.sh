ad76a926948a4324fb2040089e3ebba68ccb2e1e
nickluchen <nickluchen@hotmail.com>
2014-07-29 16:31:23 +0800

    Quick fix

--- mount_clu_homes_on_bjo.sh
+++ mount_clu_homes_on_bjo.sh
@@ -45,13 +45,13 @@ else
     then
         cmd='sudo mount'
         # ${cmd} -t cifs //10.204.5.190/clu /home/clu/clu-desktop/ -o user=clu,uid=clu
-        check_and_mount ${cmd} "-t nfs -o nolock" ${clu_desktop_ip}:/home/clu ${clu_desktop_home}
+        check_and_mount "${cmd}" "-t nfs -o nolock" ${clu_desktop_ip}:/home/clu ${clu_desktop_home}
     elif [ "`uname -s`" = "Darwin" ]
     then
         cmd=mount
-        check_and_mount ${cmd} "-t smbfs" //clu@bjo-eng-bld-01.dolby.net/clu    ${clu_bld_home}
-        check_and_mount ${cmd} "-t smbfs" //clu@bjo-file-01.dolby.net/users/clu ${clu_domain_home}
-        check_and_mount ${cmd} "-t smbfs" //clu@${clu_desktop_ip}/clu           ${clu_desktop_home}
+        check_and_mount "${cmd}" "-t smbfs" //clu@bjo-eng-bld-01.dolby.net/clu    ${clu_bld_home}
+        check_and_mount "${cmd}" "-t smbfs" //clu@bjo-file-01.dolby.net/users/clu ${clu_domain_home}
+        check_and_mount "${cmd}" "-t smbfs" //clu@${clu_desktop_ip}/clu           ${clu_desktop_home}
     fi
     mount | grep clu
 fi
