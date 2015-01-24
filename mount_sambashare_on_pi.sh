bf302ce1b623693a639be244fc497b1bbc5c10af
Chen Lu <nickluchen@hotmail.com>
2015-01-24 14:34:39 +0800

    Updated the mount commands to mount RAID on pi.

--- mount_sambashare_on_pi.sh
+++ mount_sambashare_on_pi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo mount -t ntfs-3g /dev/sda1 /home/smbshare_1 
-sudo mount -t ntfs-3g /dev/sdb1 /home/smbshare_2 
+# Before setting up the RAID
+#sudo mount -t ntfs-3g /dev/sda1 /home/smbshare_1 
+#sudo mount -t ntfs-3g /dev/sdb1 /home/smbshare_2 
 
-mount | grep "/dev/sd"
+#mount | grep "/dev/sd"
+
+sudo mount -t ext4 /dev/md0 /home/smbshare_1
+mount | grep "/dev/md"
