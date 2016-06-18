d27b8418b84567c6b28333881bef4d23c17d6d1d
Chen Lu <nickluchen@hotmail.com>
2016-06-18 17:44:36 +0800

    Add an example base python file for creating a new script.

---
+++ python_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+
+# Helper variables
+basedir = os.getcwd()
+here    = os.path.abspath(os.path.dirname(__file__))
+
+
+# Main
+def main():
+
+
+if __name__ == '__main__':
+    main()
+
